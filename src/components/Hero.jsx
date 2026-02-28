a19979fcb55b42f28bc9aa7fd4f9038dbedaadbe
Dawid Wieclaw <dawid.wieclaw@epitech.eu>
2026-02-28 20:21:10 +0100

    Hero section responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,20 +12,20 @@ export default function Hero() {
     return (
         <div className="min-h-screen text-white  font-semibold flex flex-col items-center justify-center relative bg-linear-to-br from-gray-950 via-gray-900 to-black">
 
-            <div className="flex md:text-9xl text-6xl font-bold">
+            <div className="flex md:text-9xl text-5xl font-bold">
                 <h2 className="text-gray-400">Dawid&nbsp;</h2>
                 <h2 className="text-gray-600">Wieclaw</h2>
             </div>
 
-            <p className="text-3xl text-gray-600 font-semibold">Full Stack Web Developer</p>
+            <p className="text-2xl text-gray-600 font-semibold">Full Stack Web Developer</p>
 
             <div className="flex flex-col text-gray-400 items-center mt-10 font-bold">
-                <p className="md:text-5xl text-3xl md:mt-10">RECHERCHE ALTERNANCE</p>
-                <p className="md:text-4xl text-2xl mt-2">Septembre 2026 / 14 mois</p>
+                <p className="md:text-5xl text-2xl md:mt-10">RECHERCHE ALTERNANCE</p>
+                <p className="md:text-4xl text-xl mt-2">Septembre 2026 / 14 mois</p>
             </div>
 
-            <div className="mt-20 text-black md:text-xl">
-                <button className="bg-gray-500 hover:bg-gray-300 p-3 md:px-7 px-5 rounded-xl border-black mx-6"
+            <div className="mt-20 text-black md:text-xl flex">
+                <button className="bg-gray-500 hover:bg-gray-300 p-3 md:px-7 px-4 rounded-xl border-black mx-6"
                     type="button"
                     onClick={() => scrollToSection("About")}>Voir mes projets</button>
 
