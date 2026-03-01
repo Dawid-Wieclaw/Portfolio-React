92e13e4b3fa5f804959411283aaf81feca71f8db
Dawid Wieclaw <dawid.wieclaw@epitech.eu>
2026-03-01 12:56:39 +0100

    New design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { AuroraText } from "./ui/aurora-text";
 import { TypingAnimation } from "./ui/typing-animation";
+import { DotPattern } from "./ui/dot-pattern";
 import { Github, Mail, Linkedin } from 'lucide-react';
 
 
@@ -15,48 +16,50 @@ export default function Hero() {
     }
 
     return (
-        <div className="min-h-screen text-white flex flex-col items-center justify-center relative bg-black">
 
-            <div className="flex md:text-9xl text-5xl font-bold">
-                <AuroraText>Dawid&nbsp;</AuroraText>
-                <h2 className="text-gray-400">Wieclaw</h2>
+            <div className="min-h-screen text-white flex flex-col items-center justify-center relative bg-black">
+                <DotPattern></DotPattern>
+
+                <div className="flex md:text-9xl text-5xl font-bold">
+                    <AuroraText>Dawid&nbsp;</AuroraText>
+                    <h2 className="text-gray-400">Wieclaw</h2>
+                </div>
+
+                <p className="md:text-4xl text-2xl text-gray-600 ">Web Developer</p>
+
+
+                <div className="flex flex-col text-gray-400 items-center md:mt-20 mt-5">
+                    <AuroraText className="md:text-4xl text-2xl ">RECHERCHE ALTERNANCE</AuroraText>
+                    <TypingAnimation className="md:text-3xl text-xl">Septembre 2026 / 14 mois</TypingAnimation>
+                </div>
+
+
+                <div className="mt-10 md:gap-10 gap-4.5 flex">
+                    <a className="text-gray-500 hover:text-gray-300"
+                        href="https://github.com/lblrs"
+                        target="_blank">
+                        <Github></Github>
+                    </a>
+
+                    <a className="text-gray-500 hover:text-gray-300"
+                        href=""
+                        target="_blank">
+                        <Linkedin></Linkedin>
+                    </a>
+
+                    <a className="text-gray-500 hover:text-gray-300"
+                        href="mailto:dawid.wieclaw@epitech.eu"
+                        target="_blank">
+                        <Mail></Mail>
+                    </a>
+
+                    <a className=" md:text-2xl text-xl text-center text-gray-500 hover:text-gray-300"
+                        href="src/assets/Dawid-Wieclaw-CV.pdf"
+                        target="_blank">
+                        CV
+                    </a>
+                </div>
+
             </div>
-
-            <p className="md:text-4xl text-2xl text-gray-600 ">Web Developer</p>
-
-
-            <div className="flex flex-col text-gray-400 items-center md:mt-20 mt-5">
-                <AuroraText className="md:text-4xl text-2xl ">RECHERCHE ALTERNANCE</AuroraText>
-                <TypingAnimation className="md:text-3xl text-xl">Septembre 2026 / 14 mois</TypingAnimation>
-            </div>
-
-
-            <div className="mt-10 md:gap-10 gap-4.5 flex">
-                <a className="text-gray-500 hover:text-gray-300"
-                    href="https://github.com/lblrs"
-                    target="_blank">
-                    <Github></Github>
-                </a>
-
-                <a className="text-gray-500 hover:text-gray-300"
-                    href=""
-                    target="_blank">
-                    <Linkedin></Linkedin>
-                </a>
-
-                <a className="text-gray-500 hover:text-gray-300"
-                    href="mailto:dawid.wieclaw@epitech.eu"
-                    target="_blank">
-                    <Mail></Mail>
-                </a>
-
-                <a className=" md:text-2xl text-xl text-center text-gray-500 hover:text-gray-300"
-                    href="src/assets/Dawid-Wieclaw-CV.pdf"
-                    target="_blank">
-                    CV
-                </a>
-            </div>
-
-        </div>
     )
 }
