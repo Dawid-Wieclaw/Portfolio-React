199d132041282141bc7fc783d12b363cca00bd2f
Dawid Wieclaw <dawid.wieclaw@epitech.eu>
2026-03-01 14:27:47 +0100

    New design

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,12 +8,12 @@ export default function Projects() {
                 <ScrollVelocityContainer className="opacity-50">
                     <ScrollVelocityRow baseVelocity={5}>
                         <div className="flex md:gap-20 gap-10">
-                            <img src="/img/javascript.svg" className="md:h-15 h-10"></img>
-                            <img src="/img/react.svg" className="md:h-15 h-10"></img>
-                            <img src="/img/tailwindcss-icon.svg" className="md:h-15 h-10"></img>
-                            <img src="/img/bootstrap.svg" className="md:h-15 h-10"></img>
-                            <img src="/img/php.svg" className="md:h-15 h-10"></img>
-                            <img src="/img/laravel.svg" className="md:h-15 h-10 md:mr-20 mr-10"></img>
+                            <img src="img/javascript.svg" className="md:h-15 h-10"></img>
+                            <img src="img/react.svg" className="md:h-15 h-10"></img>
+                            <img src="img/tailwindcss-icon.svg" className="md:h-15 h-10"></img>
+                            <img src="img/bootstrap.svg" className="md:h-15 h-10"></img>
+                            <img src="img/php.svg" className="md:h-15 h-10"></img>
+                            <img src="img/laravel.svg" className="md:h-15 h-10 md:mr-20 mr-10"></img>
                         </div>
                     </ScrollVelocityRow>
                 </ScrollVelocityContainer>
