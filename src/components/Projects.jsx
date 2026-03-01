f2e152e707ad7dc2c542908fcada1be1ab3a998a
Dawid Wieclaw <dawid.wieclaw@epitech.eu>
2026-03-01 18:57:27 +0100

    Improve design

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,24 +4,24 @@ import { BorderBeam } from "./ui/border-beam"
 export default function Projects() {
 
     return (
-        <div id="Projects" className="bg-black h-screen w-screen pt-25">
+        <div id="Projects" className="bg-black h-screen w-screen">
             <div className=" bg-black py-10 ">
                 <ScrollVelocityContainer className="opacity-50">
                     <ScrollVelocityRow baseVelocity={5}>
-                        <div className="flex md:gap-20 gap-10 text-white">
-                            <p className="flex items-center text-xl gap-4 font-semibold"><img src="img/javascript.svg" className="md:h-15 h-10"></img>JavaScript</p>
-                            <p className="flex items-center text-xl gap-4 font-semibold"><img src="img/react.svg" className="md:h-15 h-10"></img>React</p>
-                            <p className="flex items-center text-xl gap-4 font-semibold"><img src="img/tailwindcss-icon.svg" className="md:h-15 h-10"></img>TailwindCss</p>
-                            <p className="flex items-center text-xl gap-4 font-semibold mr-18"><img src="img/bootstrap.svg" className="md:h-15 h-10"></img>Bootstrap</p>
+                        <div className="flex md:gap-20 gap-8 text-white">
+                            <p className="flex items-center md:text-xl text-sm gap-2 font-semibold"><img src="img/javascript.svg" className="md:h-15 h-5"></img>JavaScript</p>
+                            <p className="flex items-center md:text-xl gap-2 font-semibold"><img src="img/react.svg" className="md:h-15 h-5"></img>React</p>
+                            <p className="flex items-center md:text-xl gap-2 font-semibold"><img src="img/tailwindcss-icon.svg" className="md:h-15 h-5"></img>TailwindCss</p>
+                            <p className="flex items-center md:text-xl gap-2 font-semibold md:mr-18 mr-10"><img src="img/bootstrap.svg" className="md:h-15 h-5"></img>Bootstrap</p>
                         </div>
                     </ScrollVelocityRow>
 
-                    <ScrollVelocityRow baseVelocity={-3} className="pt-10">
-                        <div className="flex md:gap-20 gap-10 text-white">
-                            <p className="flex items-center text-xl gap-4 font-semibold"><img src="img/php.svg" className="md:h-15 h-10"></img>PHP</p>
-                            <p className="flex items-center text-xl gap-4 font-semibold "><img src="img/html-5.svg" className="md:h-15 h-10"></img>HTML</p>
-                            <p className="flex items-center text-xl gap-4 font-semibold "><img src="img/laravel.svg" className="md:h-15 h-10"></img>Laravel</p>
-                            <p className="flex items-center text-xl gap-4 font-semibold mr-15"><img src="img/css-3.svg" className="md:h-15 h-10"></img>CSS</p>
+                    <ScrollVelocityRow baseVelocity={-3} className="md:pt-10 pt-5">
+                        <div className="flex md:gap-20 gap-8 text-white">
+                            <p className="flex items-center md:text-xl gap-2 font-semibold"><img src="img/php.svg" className="md:h-15 h-5"></img>PHP</p>
+                            <p className="flex items-center md:text-xl gap-2 font-semibold "><img src="img/html-5.svg" className="md:h-15 h-5"></img>HTML</p>
+                            <p className="flex items-center md:text-xl gap-2 font-semibold "><img src="img/laravel.svg" className="md:h-15 h-5"></img>Laravel</p>
+                            <p className="flex items-center md:text-xl gap-2 font-semibold md:mr-15 mr-8"><img src="img/css-3.svg" className="md:h-15 h-5"></img>CSS</p>
                         </div>
                     </ScrollVelocityRow>
                 </ScrollVelocityContainer>
