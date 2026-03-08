bada5c6750cf1a5ae0d77d02664309c548e2b0e4
Dawid Wieclaw <dawid.wieclaw@epitech.eu>
2026-03-08 12:22:43 +0100

    Mobile width project section

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -39,7 +39,7 @@ export default function Projects() {
         <div id="Projects" className="bg-black min-h-screen flex flex-col items-center">
 
 
-            <div className="max-w-screen">
+            <div className="max-w-screen pt-5">
                 <Technos></Technos>
             </div>
 
