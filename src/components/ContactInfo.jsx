60aa7c95271a9b1042179b9582e36e2acac9aec3
Dawid Wieclaw <dawid.wieclaw@epitech.eu>
2026-03-19 14:09:04 +0100

    Fix contact font size on mobile

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -8,7 +8,7 @@ export default function ContactInfo({ id, Icon, type, info, href }) {
             <div className="flex items-center rounded-xl text-gray-300 text-lg p-3 gap-10  bg-zinc-950 border border-zinc-800 hover:border-zinc-500 hover:border-2 transition">
                 <Icon size={30} className="text-gray-300" />
 
-                <div className="flex flex-col gap-3">
+                <div className="flex flex-col ">
                     <AuroraText>{type}</AuroraText>
                     <p className="2xl:text-2xl md:text-xl text-sm">{info}</p>
                 </div>
